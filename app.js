3484d6099e1c33b51bb5cf771b870483ca980099
Abhinaya <abhisonukondi123@gmail.com>
2024-12-28 00:29:59 -0500

    Made few accordian collapse cards changes -not working though lol

---
+++ app.js
@@ -0,0 +1,29 @@
+// var coll = document.getElementsByClassName("card-title");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//     coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.maxHeight){
+//       content.style.maxHeight = null;
+//     } 
+//     else {
+//       content.style.maxHeight = content.scrollHeight + "px";
+//     } 
+//   });
+// }
+
+
+// const toggles = document.querySelector(".card-title");
+// const coll = document.querySelector(".transit-card");
+// toggles.onclick = ()=>{
+//     coll.classList.add("active");
+// }
+
+
+// toggles.forEach((toggle) => {
+//     toggle.addEventListener("click", () => {
+//         toggle.parentNode.classList.toggle("active");
+//     })
+// })
