6bfdba325ea062e8933863a4d0497c1481be3125
Abhinaya <abhisonukondi123@gmail.com>
2024-12-31 00:04:01 -0500

    Changes to product card titles and collapsible

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
-// var coll = document.getElementsByClassName("card-title");
-// var i;
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-// for (i = 0; i < coll.length; i++) {
-//     coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.maxHeight){
-//       content.style.maxHeight = null;
-//     } 
-//     else {
-//       content.style.maxHeight = content.scrollHeight + "px";
-//     } 
-//   });
-// }
-
-
-// const toggles = document.querySelector(".card-title");
-// const coll = document.querySelector(".transit-card");
-// toggles.onclick = ()=>{
-//     coll.classList.add("active");
-// }
-
-
-toggles.forEach((toggle) => {
-    toggle.addEventListener("click", () => {
-        toggle.parentNode.classList.toggle("active");
-    })
-})
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
